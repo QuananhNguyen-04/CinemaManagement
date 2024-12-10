5f921f772764600c366a4fc417445d1050ac6394
QuananhNguyen-04 <sherlingfordk@gmail.com>
2024-12-11 01:29:02 +0700

    Adapt db with .env

--- config/db.js
+++ config/db.js
@@ -1,11 +1,13 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',      // Replace with your PostgreSQL username
-    host: 'localhost',         // Host where PostgreSQL is running
-    database: 'cinemamanagement', // Your database name
-    password: '221bBaker', // Replace with your PostgreSQL password
-    port: 5432,                // Default PostgreSQL port
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
