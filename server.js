d729842450efe4e485bc7cf2a5e352920bece515
QuananhNguyen-04 <sherlingfordk@gmail.com>
2024-12-12 02:33:52 +0700

    Modified CSS file for 3 pages, need further improvement

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 // Middleware to parse JSON
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
